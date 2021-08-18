85851b0775e91ccb1e32eacfd3093629503b4a96
Exr0n <mail@exr0n.com>
2021-08-18 10:23:13 -0700

    neon bindings op

---
+++ aloh-engine/src/lib.rs
@@ -0,0 +1,16 @@
+use neon::prelude::*;
+
+fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    Ok(cx.string("hello node"))
+}
+
+#[neon::main]
+fn main(mut cx: ModuleContext) -> NeonResult<()> {
+    cx.export_function("hello", hello)?;
+    //cx.export_function("scan_workspace", scan_workspace)?;
+    //cx.export_function("update_file", update_file)?;
+    //cx.export_function("genCompletions", generate_completions)?;
+    //cx.export_function("genBlurb", generate_blurb)?;
+    //cx.export_function("genSummary", generate_summary)?;
+    Ok(())
+}
---
+++ aloh-langserv/engine_glue.mjs
@@ -0,0 +1,5 @@
+// run with node --experimental-specifier-resolution=node engine_glue.mjs
+import Engine from '../aloh-engine';
+console.log(Engine);
+console.log(Engine.hello());
+
